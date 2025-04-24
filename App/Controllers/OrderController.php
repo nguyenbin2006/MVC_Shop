ab6131e25a469aba408f986c1bd977878b721d10
nguyenbin2006 <nguyentanbin2006@gmail.com>
2025-04-24 09:41:19 +0700

    update

--- App/Controllers/OrderController.php
+++ App/Controllers/OrderController.php
@@ -10,7 +10,14 @@ class OrderController
         }
         $orderModel = new OrderModel();
         $productModel = new ProductModel();
-        $orderId = $orderModel->createOrder($_SESSION['user_id'], 0);
+
+        $total = 0;
+            foreach ($_SESSION['cart'] as $item) {
+                $product = $productModel->getProductById($item['product_id']);
+                $total += $product['Price'] * $item['quantity'];
+            }
+
+        $orderId = $orderModel->createOrder($_SESSION['user_id'], $total);
         foreach ($_SESSION['cart'] as $item) {
             $product = $productModel->getProductById($item['product_id']);
             $orderModel->addOrderItem($orderId, $item['product_id'], 
@@ -20,4 +27,25 @@ class OrderController
         unset($_SESSION['cart']);
         include './App/Views/Order/checkout_success.php';
    }
+
+   public function history()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (!isset($_SESSION['user_id'])) {
+            $config = require './config.php';
+            header("Location: " . $config['baseURL'] . "user/login");
+            exit;
+        }
+
+        $orderModel = new OrderModel();
+        $orders = $orderModel->getOrdersByUserId($_SESSION['user_id']);
+
+        $config = require './config.php';
+        $baseURL = $config['baseURL'];
+
+        include './App/Views/Order/history.php';
+}
 }
--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-$configu = require_once './config.php';
+$config = require_once './config.php';
 require_once './App/Model/UserModel.php';
 
 class UserController
@@ -38,4 +38,36 @@ class UserController
         }
         include './App/Views/User/register.php';
     }   
+
+    public function login()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $error = '';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $pdo = new PDO("mysql:host=localhost;dbname=productdb", "root", "");
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+            $stmt->execute([$username]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($user && password_verify($password, $user['password'])) {
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+                $config = require 'config.php';
+                $baseURL = $config['baseURL'];
+                header("Location: " . $baseURL . 'home/index');// về trang chủ
+                exit;
+            } else {
+                $error = "Tên đăng nhập hoặc mật khẩu không đúng.";
+            }
+        }
+        include './App/Views/User/login.php';
+    }
 }
